22a430dee3727a2e2f42d570837a0ac49f9fe91f
fmoletta <fedemoletta@hotmail.com>
2025-09-02 16:39:38 -0300

    Update block number

--- cmd/ethrex/initializers.rs
+++ cmd/ethrex/initializers.rs
@@ -76,8 +76,8 @@ pub fn init_metrics(opts: &Options, tracker: TaskTracker) {
 /// Opens a new or pre-existing Store and loads the initial state provided by the network
 pub async fn init_store(data_dir: &str, genesis: Genesis) -> Store {
     let store = open_store(data_dir);
-    info!("Restoring latest block number to 8497589");
-    store.update_latest_block_number(8497589).await.unwrap();
+    info!("Restoring latest block number to 8662709");
+    store.update_latest_block_number(8662709).await.unwrap();
     store
         .add_initial_state(genesis)
         .await
